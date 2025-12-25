728022f641e6f3816bfee5dbe129e031b5d2497c
NourhanGimaey <nourhan.ahmedsaad@gmail.com>
2025-12-25 12:19:31 +0200

    Updated home carousel background

--- lib/features/main_layout/presentation/home/presentation/widgets/home_carousel_slider.dart
+++ lib/features/main_layout/presentation/home/presentation/widgets/home_carousel_slider.dart
@@ -1,28 +1,86 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies/core/theme/app_colors.dart';
+import 'package:movies/core/utils/app_assets.dart';
 import 'package:movies/features/main_layout/presentation/widgets/movie_card.dart';
 import 'package:movies/features/main_layout/data/models/movies_list_model/movies_list_model.dart';
 
-class HomeCarouselSlider extends StatelessWidget {
+class HomeCarouselSlider extends StatefulWidget {
   final Data data;
 
   const HomeCarouselSlider({super.key, required this.data});
 
   @override
+  State<HomeCarouselSlider> createState() => _HomeCarouselSliderState();
+}
+
+class _HomeCarouselSliderState extends State<HomeCarouselSlider> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: data.movies.length,
-      itemBuilder: (context, index, realIndex) => SizedBox(
-        width: double.infinity,
-        child: MovieCard(movies: data.movies[index]),
-      ),
-      options: CarouselOptions(
-        enlargeCenterPage: true,
-        enlargeFactor: 0.35,
-        viewportFraction: .6,
-        aspectRatio: 1.1,
-        initialPage: 0,
-      ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 500),
+          child: Container(
+            key: ValueKey<String>(
+              widget.data.movies[_currentIndex].largeCoverImage,
+            ),
+            height: 650.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  widget.data.movies[_currentIndex].largeCoverImage,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              width: double.infinity,
+              height: 650.h,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    AppColors.black.withValues(alpha: 0.6),
+                    AppColors.black.withValues(alpha: 0.8),
+                    AppColors.black.withValues(alpha: 1),
+                  ],
+                  stops: const [0, 0.5, 1],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(top: 50.h, child: Image.asset(AppImages.availableNow)),
+
+        CarouselSlider.builder(
+          itemCount: widget.data.movies.length,
+          itemBuilder: (context, index, realIndex) => SizedBox(
+            width: double.infinity,
+            child: MovieCard(movies: widget.data.movies[index]),
+          ),
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            enlargeFactor: 0.35,
+            viewportFraction: .6,
+            aspectRatio: 1.15,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+        ),
+
+        Positioned(bottom: 0.h, child: Image.asset(AppImages.watchNow)),
+      ],
     );
   }
 }
